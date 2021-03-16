15e6baa40a41943f8aaf774487f7515028e1736a
EdinaDinka <cuturaedina@gmail.com>
2021-03-16 13:28:52 +0100

    Added ex03

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,14 +1,13 @@
-function myEqualFunction(num) {
+function myEqualFunction(num){
     if (num === 23) {
         return "Equal";
-    } else {
+    }
         return "Not equal";
     }
-}
-console.log(myEqualFunction(5));
-console.log(myEqualFunction(23));
-console.log(myEqualFunction("23"));
-console.log(myEqualFunction('23'));
-console.log(myEqualFunction("text"));
-module.exports = myEqualFunction;
+    console.log(myEqualFunction(5));
+    console.log(myEqualFunction(23));
+    console.log(myEqualFunction("23"));
+    console.log(myEqualFunction('23'));
+    console.log(myEqualFunction("text"));
+    module.exports = myEqualFunction;
 
